c73e5425edbff2f96b366fd164fc6b5015e9e1b5
Will Constable <willconstable@gmail.com>
2018-07-27 21:57:03 +0300

    add ex 4.9, runs but not correct

---
+++ example_4.9_gamblers.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def initial_value():
+    v = np.random.random(101)
+    v[0] = 0
+    v[100] = 1
+    return v
+
+
+def initial_policy(states):
+    p = [0] + [np.random.randint(1, max_stakes(s) + 1) for s in states[1:-1]] + [0]
+    return np.array(p)
+
+
+def max_stakes(capital):
+    return min(capital, 100-capital)
+
+
+def states():
+    return range(0, 101)
+
+
+def possible_actions(state):
+    return range(1, max_stakes(state))
+
+def evaluate_policy(policy, values, states, P_WIN):
+    new_values = values.copy()
+    for s in states[1:-1]:
+        a = policy[s]
+        reward = 1 if s + a == 100 else 0
+        new_values[s] = P_WIN * (reward + values[s + a]) + \
+                        (1 - P_WIN) * (0 + values[s - a])
+    return new_values
+
+
+def improve_policy(policy, values, states, P_WIN):
+    new_policy = policy.copy()
+    for s in states[1:-1]:
+        best_action_value = 0
+        for a in possible_actions(s):
+            action_value = 0
+            for p, new_s in zip([P_WIN, 1 - P_WIN], [s + a, s - a]):
+                action_value += p * values[new_s]
+            if action_value > best_action_value:
+                best_action_value = action_value
+                new_policy[s] = a
+    return new_policy
+
+
+def train(P_WIN=0.4):
+    S_plus = states()
+    V = initial_value() 
+    P = initial_policy(S_plus)
+
+    for i in range(1000):
+        V = evaluate_policy(P, V, S_plus, P_WIN)
+        P = improve_policy(P, V, S_plus, P_WIN) 
+
+    print V
+    print P
+
+if __name__ == "__main__":
+    
+    train()
