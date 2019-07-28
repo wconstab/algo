aed69aead819fe4c47788f30c3ea868ea403adca
Will Constable <willconstable@gmail.com>
2019-07-28 14:34:22 -0700

    much faster with simple heuristic

--- aoc/day5/solve_day5.py
+++ aoc/day5/solve_day5.py
@@ -19,19 +19,24 @@ def solve_day_5_part_1(input_str):
 
 	while progress:
 		progress = False
-		for i in range(len(input)-1):
+		i = 0
+		while i < len(input) - 1:
 			if react(input[i], input[i+1]):
 				progress = True
 				input.pop(i)
 				input.pop(i)
-				break
+				# Heuristic: the reaction might have opened a new possibility one to the left
+				if i > 0:
+					i -= 1
+			else:
+				i += 1
 
 	return "".join(input)
 
 
 if __name__ == "__main__":
 	for t, a in test_input:
-		result = solve_day_5(t)
+		result = solve_day_5_part_1(t)
 		assert a == result, "expected {} actual {}".format(a, result)
 
 	real_input = load_input("input")
